1c957c36a8e29eef6b10732b03257046eaf2b2e2
Μιχαίας <mmcadoulos@gmail.com>
2024-05-02 16:01:02 -0500

    Video: 1.2

---
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -0,0 +1,36 @@
+package org.launchcode.codingevents.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("events")
+public class EventController {
+
+    @GetMapping()
+    public String getEvent(Model model){
+        List<String> events = new ArrayList<>();
+        events.add("Event 1");
+        events.add("Event 2");
+        events.add("Event 3");
+        events.add("Event 4");
+
+        model.addAttribute("events", events);
+
+        return "events/index";
+    }
+
+    // lives at /events/create
+    @GetMapping("create")
+    public String renderCreateEventForm(){
+        return "events/create";
+    }
+
+
+}
