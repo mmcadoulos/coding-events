4752b72839e47d5e22a701aaf08de55fa2bbbdd1
Μιχαίας <mmcadoulos@gmail.com>
2024-05-02 17:24:10 -0500

    1.3

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,13 +14,15 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    private static List<String> events = new ArrayList<>();
+
     @GetMapping()
     public String getEvent(Model model){
-        List<String> events = new ArrayList<>();
-        events.add("Event 1");
-        events.add("Event 2");
-        events.add("Event 3");
-        events.add("Event 4");
+//        List<String> events = new ArrayList<>();
+//        events.add("Event 1");
+//        events.add("Event 2");
+//        events.add("Event 3");
+//        events.add("Event 4");
 
         model.addAttribute("events", events);
 
@@ -32,5 +35,9 @@ public class EventController {
         return "events/create";
     }
 
-
+@PostMapping("create")
+    public String createEvent(@RequestParam String eventName){
+        events.add(eventName);
+        return "redirect:/events";
+}
 }
