f8eabf7c74621f24307f98c356e7d2deb7b03be0
Μιχαίας <mmcadoulos@gmail.com>
2024-05-06 18:31:41 -0500

    2.4

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,5 +1,6 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,50 +9,32 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-    private static ArrayList<Event> events = new ArrayList<>();
-
     @GetMapping()
     public String getEvent(Model model) {
-//        List<String> events = new ArrayList<>();
-//        events.add("Event 1");
-//        events.add("Event 2");
-//        events.add("Event 3");
-//        events.add("Event 4");
-//        Event event1 = new Event("Nap-Time", "You get to take a nap guilt-free!");
-//        Event event2 = new Event("Coffee and Code", "Code with some people and drink nasty beverages");
-//        Event event3 = new Event("Bar-Be-Code", "Code with fingers stained with delicious bbq");
-//        events.add(event1);
-//        events.add(event2);
-//        events.add(event3);
-
-        model.addAttribute("events", events);
-
+        model.addAttribute("title", "All Events");
+        model.addAttribute("events", EventData.getAll());
         return "events/index";
     }
 
-    // lives at /events/create
     @GetMapping("create")
-    public String renderCreateEventForm() {
+    public String renderCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Event");
         return "events/create";
     }
 
     @PostMapping("create")
     public String createEvent(@RequestParam String eventName, @RequestParam String eventInfo) {
-
         if (eventName.isBlank()) {
             return "redirect:/events";
         } else if (eventInfo.isBlank()) {
-            events.add(new Event(eventName));
+            EventData.addEvent(new Event(eventName));
         } else {
-            events.add(new Event(eventName, eventInfo));
+            EventData.addEvent(new Event(eventName, eventInfo));
         }
         return "redirect:/events";
     }
--- src/main/java/org/launchcode/codingevents/controllers/HomeController.java
+++ src/main/java/org/launchcode/codingevents/controllers/HomeController.java
@@ -1,14 +1,16 @@
 package org.launchcode.codingevents.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String getIndex() {
-    return "index";
+    public String getIndex(Model model) {
+        model.addAttribute("title", "Coding Events");
+        return "index";
     }
 
 }
